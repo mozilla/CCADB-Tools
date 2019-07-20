74a653a24d7a3362fde77be6d9450e6da42e532d
Christopher Henderson <chris@chenderson.org>
2019-07-20 13:52:29 -0700

    old fashioned

--- kintoDiffCCADB/src/firefox/mod.rs
+++ kintoDiffCCADB/src/firefox/mod.rs
@@ -152,6 +152,7 @@ mod tests {
 //        fs_extra::dir::copy(ff.home.path(), "/home/chris/ff",  &fs_extra::dir::CopyOptions::new());
         let profile = ff.create_profile().unwrap();
         println!("GETTING OUT! {}", profile.home.path().to_string_lossy());
+        std::thread::sleep(Duration::from_secs(60*5));
         fs_extra::dir::copy(profile.home.path(), "/home/chris/pwease", &fs_extra::dir::CopyOptions::new());
     }
 }
