2178d883777f09ed518d6bb5ffd89a18ea6fe9cd
Christopher Henderson <chris@chenderson.org>
2019-11-16 12:31:17 -0800

    is dir

--- kinto_integrity/src/firefox/cert_storage/mod.rs
+++ kinto_integrity/src/firefox/cert_storage/mod.rs
@@ -32,7 +32,8 @@ impl TryFrom<PathBuf> for CertStorage {
         let mut builder = Rkv::environment_builder();
         builder.set_max_dbs(2);
         builder.set_flags(EnvironmentFlags::READ_ONLY);
-        info!("{}", db_path.is_dir());
+
+        info!("{}", db_path.as_path().is_dir());
         let env = Rkv::from_env(&db_path, builder)?;
         let store = env.open_single("cert_storage", StoreOptions::default())?;
         let reader = env.read()?;
