73bb3089a06a5247c60bc3b957aced8733c41663
christopher-henderson <chris@chenderson.org>
2020-05-02 13:41:00 -0700

    explicit 404

--- kinto_integrity/src/nginx/mod.rs
+++ kinto_integrity/src/nginx/mod.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use crate::errors::*;
 use crate::configuration;
 
@@ -56,6 +60,10 @@ server {{
 
 	location ~ ^/{locations} {{
 		proxy_pass http://backend;
+	}}
+
+	location / {{
+	    return 404;
 	}}
 
 }}
